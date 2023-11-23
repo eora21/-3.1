8fb483b73b8a470d5aca2e6e057ae6a596ad1a63
eora21 <eora21@naver.com>
2023-11-23 21:10:44 +0900

    feat: 🎸 추가 유저 정보 등록, 조회 코드 작성

---
+++ before_spring/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.sql.SQLException;
+import user.dao.UserDao;
+import user.domain.User;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("eora21");
+        user.setName("김주호");
+        user.setPassword("pswd");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User getUser = dao.get(user.getId());
+        System.out.println(getUser.getName());
+        System.out.println(getUser.getPassword());
+
+        System.out.println(getUser.getId() + " 조회 성공");
+    }
+}
