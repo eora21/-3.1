f0b1a3ce5156d091c26ce1b04daeb47fed1d5c8a
eora21 <eora21@naver.com>
2023-11-24 11:54:25 +0900

    refactor: 💡 수정 인터페이스 도입

--- before_spring/src/main/java/user/dao/NConnectionMaker.java
+++ before_spring/src/main/java/user/dao/NConnectionMaker.java
@@ -5,7 +5,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class SimpleConnectionMaker {
+public class NConnectionMaker implements ConnectionMaker {
+    @Override
     public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         return DriverManager.getConnection("jdbc:mysql://localhost/toby_spring",
