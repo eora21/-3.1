c9df10ec314c4ea7408a9a9b9ff47c16a7e1ee2f
eora21 <eora21@naver.com>
2023-11-24 14:59:40 +0900

    refactor: 💡 수정 애플리케이션 컨텍스트를 적용하여 객체 불러오기

--- before_spring/src/main/java/Main.java
+++ before_spring/src/main/java/Main.java
@@ -1,12 +1,14 @@
 import java.sql.SQLException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import user.dao.DaoFactory;
-import user.dao.NConnectionMaker;
 import user.dao.UserDao;
 import user.domain.User;
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("eora21");
