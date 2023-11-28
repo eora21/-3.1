f35b326f6b907f533c5711506c25be62801bee4b
eora21 <eora21@naver.com>
2023-11-28 17:36:16 +0900

    test: 💍 수정 테스트 통합 및 get() 테스트 기능 보완

--- before_spring/src/test/java/UserDaoTest.java
+++ before_spring/src/test/java/UserDaoTest.java
@@ -1,76 +1,35 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.SQLException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
-import user.dao.CountingConnectionMaker;
-import user.dao.CountingDaoFactory;
-import user.dao.DaoFactory;
 import user.dao.UserDao;
 import user.domain.User;
 
 public class UserDaoTest {
     @Test
-    void daoTest() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+    void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User userA = new User("A", "에이", "PA");
+        User userB = new User("B", "비", "PB");
 
         dao.deleteAll();
         assertThat(dao.getCount()).isZero();
 
-        User user = new User();
-        user.setId("eora21");
-        user.setName("김주호");
-        user.setPassword("pswd");
+        dao.add(userA);
+        dao.add(userB);
+        assertThat(dao.getCount()).isEqualTo(2);
 
-        dao.add(user);
-        assertThat(dao.getCount()).isOne();
+        User getUserA = dao.get(userA.getId());
+        assertThat(userA.getName()).isEqualTo(getUserA.getName());
+        assertThat(userA.getPassword()).isEqualTo(getUserA.getPassword());
 
-        User getUser = dao.get(user.getId());
-        assertThat(user.getId()).isEqualTo(getUser.getId());
-        assertThat(user.getPassword()).isEqualTo(getUser.getPassword());
-    }
-
-    @Test
-    void connectionCountingTest() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        User user = new User();
-        user.setId("eora21");
-        user.setName("김주호");
-        user.setPassword("pswd");
-
-        dao.add(user);
-
-        User getUser = dao.get(user.getId());
-        assertThat(user.getId()).isEqualTo(getUser.getId());
-        assertThat(user.getPassword()).isEqualTo(getUser.getPassword());
-
-        CountingConnectionMaker connectionMaker =
-                context.getBean("connectionMaker", CountingConnectionMaker.class);
-
-        assertThat(connectionMaker.getCounter()).isEqualTo(2);
-    }
-
-    @Test
-    void applicationContextTest() throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao", UserDao.class);
-
-        User user = new User();
-        user.setId("eora21");
-        user.setName("김주호");
-        user.setPassword("pswd");
-
-        dao.add(user);
-
-        User getUser = dao.get(user.getId());
-        assertThat(user.getId()).isEqualTo(getUser.getId());
-        assertThat(user.getPassword()).isEqualTo(getUser.getPassword());
+        User getUserB = dao.get(userB.getId());
+        assertThat(userB.getName()).isEqualTo(getUserB.getName());
+        assertThat(userB.getPassword()).isEqualTo(getUserB.getPassword());
     }
 
     @Test
