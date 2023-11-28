30e3e2655c6b3eb018d775671a03f96cabae0b2a
eora21 <eora21@naver.com>
2023-11-28 11:04:26 +0900

    test: 💍 추가 applicationContext로 빈 설정 테스트 작성

--- before_spring/src/test/java/UserDaoTest.java
+++ before_spring/src/test/java/UserDaoTest.java
@@ -2,6 +2,8 @@ import java.sql.SQLException;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import user.dao.CountingConnectionMaker;
 import user.dao.CountingDaoFactory;
 import user.dao.DaoFactory;
@@ -54,5 +56,26 @@ public class UserDaoTest {
                 context.getBean("connectionMaker", CountingConnectionMaker.class);
 
         System.out.println("연결 횟수: " + connectionMaker.getCounter());
+    }
+
+    @Test
+    void applicationContextTest() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("eora21");
+        user.setName("김주호");
+        user.setPassword("pswd");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User getUser = dao.get(user.getId());
+        System.out.println(getUser.getName());
+        System.out.println(getUser.getPassword());
+
+        System.out.println(getUser.getId() + " 조회 성공");
     }
 }
