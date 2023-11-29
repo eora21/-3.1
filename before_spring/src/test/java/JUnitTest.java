582ccddb0bbed6987e6748b2b30a499c82b63778
eora21 <eora21@naver.com>
2023-11-29 15:20:40 +0900

    test: 💍 추가 스프링 테스트 컨텍스트에 대한 학습 테스트 작성

--- before_spring/src/test/java/JUnitTest.java
+++ before_spring/src/test/java/JUnitTest.java
@@ -3,25 +3,45 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.HashSet;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:junit.xml")
 public class JUnitTest {
+    @Autowired
+    ApplicationContext context;
+
     static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
     void test1() {
         assertThat(this).isNotIn(testObjects);
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 
     @Test
     void test2() {
         assertThat(this).isNotIn(testObjects);
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 
     @Test
     void test3() {
         assertThat(this).isNotIn(testObjects);
         testObjects.add(this);
+
+        assertThat(contextObject == null || contextObject == this.context).isTrue();
+        contextObject = this.context;
     }
 }
