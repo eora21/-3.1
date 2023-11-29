8b3d34a3b565369660d1a861af0362745fd7a6bb
eora21 <eora21@naver.com>
2023-11-29 15:06:11 +0900

    test: 💍 수정 개선한 JUnit 테스트 오브젝트 생성에 대한 학습 테스트 작성

--- before_spring/src/test/java/JUnitTest.java
+++ before_spring/src/test/java/JUnitTest.java
@@ -1,25 +1,27 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 public class JUnitTest {
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObjects = new HashSet<>();
 
     @Test
     void test1() {
-        assertThat(this).isNotEqualTo(testObject);
-        testObject = this;
+        assertThat(this).isNotIn(testObjects);
+        testObjects.add(this);
     }
 
     @Test
     void test2() {
-        assertThat(this).isNotEqualTo(testObject);
-        testObject = this;
+        assertThat(this).isNotIn(testObjects);
+        testObjects.add(this);
     }
 
     @Test
     void test3() {
-        assertThat(this).isNotEqualTo(testObject);
-        testObject = this;
+        assertThat(this).isNotIn(testObjects);
+        testObjects.add(this);
     }
 }
