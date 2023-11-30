0e306634a97286f55c70c8eb10ab83c23bda62b3
eora21 <eora21@naver.com>
2023-11-30 20:04:53 +0900

    feat: 🎸 추가 콜백으로 전달된 전략을 수행하는 메서드 작성

--- before_spring/src/main/java/learningtest/template/Calculator.java
+++ before_spring/src/main/java/learningtest/template/Calculator.java
@@ -17,4 +17,10 @@ public class Calculator {
             return sum;
         }
     }
+
+    public int fileReadTemplate(String filePath, BufferedReaderCallback callback) throws IOException {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+            return callback.doSomethingWithReader(bufferedReader);
+        }
+    }
 }
