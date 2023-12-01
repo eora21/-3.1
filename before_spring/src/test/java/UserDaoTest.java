9e924b44a9b4103192b74449daca975eb4047fab
eora21 <eora21@naver.com>
2023-12-01 09:56:56 +0900

    test: 💍 추가 모든 유저 데이터를 가져오는 테스트코드 작성

--- before_spring/src/test/java/UserDaoTest.java
+++ before_spring/src/test/java/UserDaoTest.java
@@ -1,17 +1,15 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import info.Info;
 import java.sql.SQLException;
-import javax.sql.DataSource;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.datasource.SingleConnectionDataSource;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import user.dao.UserDao;
@@ -74,5 +72,40 @@ public class UserDaoTest {
     void getUserFailure() {
         assertThatThrownBy(() -> dao.get("unknownId"))
                 .isExactlyInstanceOf(EmptyResultDataAccessException.class);
+    }
+
+    @Test
+    void getAll() {
+        User userA = new User("A", "에이", "PA");
+        User userB = new User("B", "비", "PB");
+        User userC = new User("C", "씨", "PC");
+
+        List<User> users;
+
+        dao.add(userB);
+        users = dao.getAll();
+        assertThat(users).hasSize(1);
+        checkSameUser(userB, users.get(0));
+
+        dao.add(userC);
+        users = dao.getAll();
+        assertThat(users).hasSize(2);
+        checkSameUser(userB, users.get(0));
+        checkSameUser(userC, users.get(1));
+
+        dao.add(userA);
+        users = dao.getAll();
+        assertThat(users).hasSize(3);
+        checkSameUser(userA, users.get(0));
+        checkSameUser(userB, users.get(1));
+        checkSameUser(userC, users.get(2));
+    }
+
+    private void checkSameUser(User firstUser, User secondUser) {
+        assertAll(
+                () -> assertThat(firstUser.getId()).isEqualTo(secondUser.getId()),
+                () -> assertThat(firstUser.getName()).isEqualTo(secondUser.getName()),
+                () -> assertThat(firstUser.getPassword()).isEqualTo(secondUser.getPassword())
+        );
     }
 }
