1ca8d76f7720df401fe839df5ca32fc2e92b4df6
eora21 <eora21@naver.com>
2023-12-01 16:19:37 +0900

    refactor: 💡 수정 유저 획득 및 추가 시 레벨, 로그인 횟수, 추천 횟수 반영되도록 변경

--- before_spring/src/main/java/user/dao/UserDaoJdbc.java
+++ before_spring/src/main/java/user/dao/UserDaoJdbc.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import user.domain.Level;
 import user.domain.User;
 
 public class UserDaoJdbc implements UserDao {
@@ -13,6 +14,9 @@ public class UserDaoJdbc implements UserDao {
         user.setId(rs.getString("id"));
         user.setName(rs.getString("name"));
         user.setPassword(rs.getString("password"));
+        user.setLevel(Level.valueOf(rs.getInt("level")));
+        user.setLogin(rs.getInt("login"));
+        user.setRecommend(rs.getInt("recommend"));
 
         return user;
     };
@@ -24,8 +28,9 @@ public class UserDaoJdbc implements UserDao {
     }
 
     public void add(User user) {
-        jdbcTemplate.update("insert into users(id, name, password) values (?,?,?)",
-                user.getId(), user.getName(), user.getPassword());
+        jdbcTemplate.update("insert into users(id, name, password, level, login, recommend) values (?,?,?,?,?,?)",
+                user.getId(), user.getName(), user.getPassword(), user.getLevel().intValue(), user.getLogin(),
+                user.getRecommend());
     }
 
     public User get(String id) {
