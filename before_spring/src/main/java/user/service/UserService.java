1745b47ede5a854a36a64f816754acab57f72fc5
eora21 <eora21@naver.com>
2023-12-02 21:43:05 +0900

    refactor: 💡 수정 트랜잭션 구현을 위해 한 커넥션에서 로직을 처리하도록 변경

--- before_spring/src/main/java/user/service/UserService.java
+++ before_spring/src/main/java/user/service/UserService.java
@@ -2,7 +2,11 @@ package user.service;
 
 import static user.domain.Level.BASIC;
 
+import java.sql.Connection;
 import java.util.Objects;
+import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import user.dao.UserDao;
 import user.domain.User;
 import user.domain.UserLevelUpgradePolicy;
@@ -10,6 +14,7 @@ import user.domain.UserLevelUpgradePolicy;
 public class UserService {
     UserLevelUpgradePolicy levelUpgradePolicy;
     UserDao userDao;
+    DataSource dataSource;
 
     public void setLevelUpgradePolicy(UserLevelUpgradePolicy levelUpgradePolicy) {
         this.levelUpgradePolicy = levelUpgradePolicy;
@@ -19,10 +24,28 @@ public class UserService {
         this.userDao = userDao;
     }
 
-    public void upgradeLevels() {
-        userDao.getAll().stream()
-                .filter(levelUpgradePolicy::canUpgradeLevel)
-                .forEach(levelUpgradePolicy::upgradeLevel);
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void upgradeLevels() throws Exception {
+        TransactionSynchronizationManager.initSynchronization();
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        connection.setAutoCommit(false);
+
+        try {
+            userDao.getAll().stream()
+                    .filter(levelUpgradePolicy::canUpgradeLevel)
+                    .forEach(levelUpgradePolicy::upgradeLevel);
+            connection.commit();
+        } catch (Exception e) {
+            connection.rollback();
+            throw e;
+        } finally {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+            TransactionSynchronizationManager.unbindResource(dataSource);
+            TransactionSynchronizationManager.clearSynchronization();
+        }
     }
 
     public void add(User user) {
