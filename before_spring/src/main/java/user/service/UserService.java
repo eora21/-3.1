db42183992b21095f97984113acd2f704ba4df42
eora21 <eora21@naver.com>
2023-12-02 10:11:11 +0900

    refactor: 💡 (UserService) 수정 유저 레벨 업은 레벨 업 정책에 맡기도록 변경

--- before_spring/src/main/java/user/service/UserService.java
+++ before_spring/src/main/java/user/service/UserService.java
@@ -1,52 +1,28 @@
 package user.service;
 
 import static user.domain.Level.BASIC;
-import static user.domain.Level.GOLD;
-import static user.domain.Level.SILVER;
 
-import java.util.EnumMap;
-import java.util.Map;
 import java.util.Objects;
-import java.util.function.Predicate;
 import user.dao.UserDao;
-import user.domain.Level;
 import user.domain.User;
+import user.domain.UserLevelUpgradePolicy;
 
 public class UserService {
-    public static final int MIN_LOGIN_COUNT_FOR_SILVER = 50;
-    public static final int MIN_RECOMMEND_COUNT_FOR_GOLD = 30;
-    private final Map<Level, Predicate<User>> levelUpConditions = writeCondition();
-
+    UserLevelUpgradePolicy levelUpgradePolicy;
     UserDao userDao;
+
+    public void setLevelUpgradePolicy(UserLevelUpgradePolicy levelUpgradePolicy) {
+        this.levelUpgradePolicy = levelUpgradePolicy;
+    }
 
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
 
-    private Map<Level, Predicate<User>> writeCondition() {
-        Map<Level, Predicate<User>> conditions = new EnumMap<>(Level.class);
-        conditions.put(BASIC, user -> user.getLogin() >= MIN_LOGIN_COUNT_FOR_SILVER);
-        conditions.put(SILVER, user -> user.getRecommend() >= MIN_RECOMMEND_COUNT_FOR_GOLD);
-        conditions.put(GOLD, user -> false);
-
-        assert conditions.size() == Level.values().length;
-        return conditions;
-    }
-
     public void upgradeLevels() {
         userDao.getAll().stream()
-                .filter(this::canLevelUp)
-                .forEach(this::levelUp);
-    }
-
-    private boolean canLevelUp(User user) {
-        return levelUpConditions.get(user.getLevel())
-                .test(user);
-    }
-
-    private void levelUp(User user) {
-        user.upgradeLevel();
-        userDao.update(user);
+                .filter(levelUpgradePolicy::canUpgradeLevel)
+                .forEach(levelUpgradePolicy::upgradeLevel);
     }
 
     public void add(User user) {
