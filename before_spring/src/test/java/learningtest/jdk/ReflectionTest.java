f72120f38a8991376cd33b9f74559a5f13924992
eora21 <eora21@naver.com>
2023-12-09 21:11:18 +0900

    test: 💍 추가 리플렉션 학습 테스트 작성

---
+++ before_spring/src/test/java/learningtest/jdk/ReflectionTest.java
@@ -0,0 +1,23 @@
+package learningtest.jdk;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.Test;
+
+public class ReflectionTest {
+    @Test
+    void invokeMethod() throws Exception {
+        String name = "Spring";
+
+        assertThat(name.length()).isEqualTo(6);
+
+        Method lengthMethod = String.class.getMethod("length");
+        assertThat(lengthMethod.invoke(name)).isEqualTo(6);
+
+        assertThat(name.charAt(0)).isEqualTo('S');
+
+        Method charAtMethod = String.class.getMethod("charAt", int.class);
+        assertThat(charAtMethod.invoke(name, 0)).isEqualTo('S');
+    }
+}
