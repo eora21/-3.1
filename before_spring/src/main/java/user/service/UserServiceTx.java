1573628cdaf816dd7a2da6de7f968e285962c5d8
eora21 <eora21@naver.com>
2023-12-09 17:39:23 +0900

    feat: 🎸 추가 트랜잭션 설정 및 유저 서비스 로직을 위임받는 클래스 작성

---
+++ before_spring/src/main/java/user/service/UserServiceTx.java
@@ -0,0 +1,37 @@
+package user.service;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import user.domain.User;
+
+public class UserServiceTx implements UserService {
+    private PlatformTransactionManager transactionManager;
+    private UserService userService;
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public void add(User user) {
+        userService.add(user);
+    }
+
+    @Override
+    public void upgradeLevels() {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            userService.upgradeLevels();
+            transactionManager.commit(status);
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw e;
+        }
+    }
+}
