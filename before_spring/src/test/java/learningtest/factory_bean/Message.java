923950021d6638791a8fc9722636587071b68955
eora21 <eora21@naver.com>
2023-12-10 10:31:10 +0900

    feat: 🎸 추가 정적 팩터리 메서드 패턴을 지니는 메시지 클래스 작성

---
+++ before_spring/src/test/java/learningtest/factory_bean/Message.java
@@ -0,0 +1,17 @@
+package learningtest.factory_bean;
+
+public class Message {
+    private final String text;
+
+    private Message(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public static Message newMessage(String text) {
+        return new Message(text);
+    }
+}
