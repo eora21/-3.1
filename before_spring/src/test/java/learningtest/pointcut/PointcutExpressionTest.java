1f668e088cbdf72a1077fdb24c169fd988c4e894
eora21 <eora21@naver.com>
2023-12-11 08:43:22 +0900

    test: 💍 추가 메서드 시그니처를 이용한 포인트컷 표현식 테스트 작성

--- before_spring/src/test/java/learningtest/pointcut/PointcutExpressionTest.java
+++ before_spring/src/test/java/learningtest/pointcut/PointcutExpressionTest.java
@@ -1,10 +1,32 @@
 package learningtest.pointcut;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 
 class PointcutExpressionTest {
     @Test
     void showMethodFullSignature() throws NoSuchMethodException {
         System.out.println(Target.class.getMethod("minus", int.class, int.class));
+    }
+
+    @Test
+    void methodSignaturePointcut() throws SecurityException, NoSuchMethodException {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution("
+                + "public int learningtest.pointcut.Target.minus(int, int) throws java.lang.RuntimeException)");
+
+        assertThat(pointcut.getClassFilter().matches(Target.class) &&
+                pointcut.getMethodMatcher().matches(Target.class.getMethod("minus", int.class, int.class), null))
+                .isTrue();
+
+        assertThat(pointcut.getClassFilter().matches(Target.class) &&
+                pointcut.getMethodMatcher().matches(Target.class.getMethod("plus", int.class, int.class), null))
+                .isFalse();
+
+        assertThat(pointcut.getClassFilter().matches(Bean.class) &&
+                pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null))
+                .isFalse();
     }
 }
