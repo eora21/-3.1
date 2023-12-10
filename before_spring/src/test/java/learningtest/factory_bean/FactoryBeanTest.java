d393490bf338610b67fedf79256578a66c426c56
eora21 <eora21@naver.com>
2023-12-10 10:49:16 +0900

    test: 💍 추가 팩토리 빈 테스트 작성

---
+++ before_spring/src/test/java/learningtest/factory_bean/FactoryBeanTest.java
@@ -0,0 +1,24 @@
+package learningtest.factory_bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "classpath:FactoryBeanTest-context.xml")
+public class FactoryBeanTest {
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    void getMessageFromFactoryBean() {
+        Object message = context.getBean("message");
+        assertThat(message).isExactlyInstanceOf(Message.class);
+        assertThat(((Message)message).getText()).isEqualTo("Factory Bean");
+    }
+}
