9012a8fb6ca03f10b28f8d1b2f647bd809fb4e28
eora21 <eora21@naver.com>
2023-12-11 12:07:02 +0900

    feat: 🎸 추가 유저 서비스 인터페이스에 추가된 메서드 구현

--- before_spring/src/main/java/user/service/UserServiceImpl.java
+++ before_spring/src/main/java/user/service/UserServiceImpl.java
@@ -2,6 +2,7 @@ package user.service;
 
 import static user.domain.Level.BASIC;
 
+import java.util.List;
 import java.util.Objects;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -23,19 +24,39 @@ public class UserServiceImpl implements UserService {
         this.userDao = userDao;
     }
 
-
-
-    public void upgradeLevels() {
-        userDao.getAll().stream()
-                .filter(levelUpgradePolicy::canUpgradeLevel)
-                .forEach(levelUpgradePolicy::upgradeLevel);
-    }
-
+    @Override
     public void add(User user) {
         if (Objects.isNull(user.getLevel())) {
             user.setLevel(BASIC);
         }
 
         userDao.add(user);
+    }
+
+    @Override
+    public User get(String id) {
+        return userDao.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+
+    @Override
+    public void deleteAll() {
+        userDao.deleteAll();
+    }
+
+    @Override
+    public void update(User user) {
+        userDao.update(user);
+    }
+
+    @Override
+    public void upgradeLevels() {
+        userDao.getAll().stream()
+                .filter(levelUpgradePolicy::canUpgradeLevel)
+                .forEach(levelUpgradePolicy::upgradeLevel);
     }
 }
