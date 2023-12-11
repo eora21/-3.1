8cac5fefc29e95285cdd3c14ed91bde3fd652a4b
eora21 <eora21@naver.com>
2023-12-11 16:49:09 +0900

    feat: 🎸 추가 유저 서비스 인터페이스에 트랜잭션 어노테이션 작성

--- before_spring/src/main/java/user/service/UserService.java
+++ before_spring/src/main/java/user/service/UserService.java
@@ -1,13 +1,17 @@
 package user.service;
 
 import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 import user.domain.User;
 
+@Transactional
 public interface UserService {
     void add(User user);
 
+    @Transactional(readOnly = true)
     User get(String id);
 
+    @Transactional(readOnly = true)
     List<User> getAll();
 
     void deleteAll();
