a88116fdddd93b2da18d46151ed5666f2578c0be
eora21 <eora21@naver.com>
2023-12-11 12:05:16 +0900

    feat: 🎸 추가 서비스를 통해서만 Dao 접근하는 구조를 위해 필요한 인터페이스 메서드 작성

--- before_spring/src/main/java/user/service/UserService.java
+++ before_spring/src/main/java/user/service/UserService.java
@@ -1,9 +1,18 @@
 package user.service;
 
+import java.util.List;
 import user.domain.User;
 
 public interface UserService {
     void add(User user);
+
+    User get(String id);
+
+    List<User> getAll();
+
+    void deleteAll();
+
+    void update(User user);
 
     void upgradeLevels();
 }
