10050f67f4a984483bc668819cbe1c8d5dbae8e2
eora21 <eora21@naver.com>
2023-12-12 10:59:43 +0900

    feat: 🎸 추가 Map을 이용한 SqlService 구현

---
+++ before_spring/src/main/java/user/sqlservice/SimpleSqlService.java
@@ -0,0 +1,23 @@
+package user.sqlservice;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class SimpleSqlService implements SqlService {
+    private Map<String, String> sqlMap;
+
+    public void setSqlMap(Map<String, String> sqlMap) {
+        this.sqlMap = sqlMap;
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+        String sql = sqlMap.get(key);
+
+        if (Objects.isNull(sql)) {
+            throw new SqlRetrievalFailureException(key + "에 대한 SQL을 찾을 수 없습니다.");
+        }
+
+        return sql;
+    }
+}
