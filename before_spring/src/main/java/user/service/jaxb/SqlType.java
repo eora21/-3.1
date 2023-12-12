5dc846317efd2a16dcc4313fec55bbad1218fc10
eora21 <eora21@naver.com>
2023-12-12 15:14:35 +0900

    fix: 🐛 삭제 불필요한 XmlRootElement 어노테이션 제거

--- before_spring/src/main/java/user/service/jaxb/SqlType.java
+++ before_spring/src/main/java/user/service/jaxb/SqlType.java
@@ -3,13 +3,11 @@ package user.service.jaxb;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlAttribute;
-import jakarta.xml.bind.annotation.XmlRootElement;
 import jakarta.xml.bind.annotation.XmlType;
 import jakarta.xml.bind.annotation.XmlValue;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "sqlType", propOrder = {"value"})
-@XmlRootElement(name = "sqlmap")
 public class SqlType {
     @XmlValue
     protected String value;
