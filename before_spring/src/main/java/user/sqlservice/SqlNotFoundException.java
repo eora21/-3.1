54796891c620cdc86182ad4ac0c46cd7820018d1
eora21 <eora21@naver.com>
2023-12-12 20:31:27 +0900

    refactor: 💡 수정 외부에서 예외 메시지를 전달받도록 변경

--- before_spring/src/main/java/user/sqlservice/SqlNotFoundException.java
+++ before_spring/src/main/java/user/sqlservice/SqlNotFoundException.java
@@ -1,7 +1,7 @@
 package user.sqlservice;
 
 public class SqlNotFoundException extends RuntimeException {
-    public SqlNotFoundException() {
-        super("SQL을 찾을 수 없습니다. 등록된 SQL이 아닙니다.");
+    public SqlNotFoundException(String message) {
+        super(message);
     }
 }
