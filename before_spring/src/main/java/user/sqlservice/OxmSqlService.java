32a22f00a4e83cad2df0e7415dbe9a3203bc2e3d
eora21 <eora21@naver.com>
2023-12-13 18:59:14 +0900

    refactor: 💡 수정 sql 정보를 주어진 형식이 아닌 Resource로 받게끔 변경

--- before_spring/src/main/java/user/sqlservice/OxmSqlService.java
+++ before_spring/src/main/java/user/sqlservice/OxmSqlService.java
@@ -1,10 +1,10 @@
 package user.sqlservice;
 
 import jakarta.annotation.PostConstruct;
-import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBException;
 import java.io.IOException;
 import javax.xml.transform.stream.StreamSource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.oxm.Unmarshaller;
 import user.sqlservice.jaxb.SqlType;
 import user.sqlservice.jaxb.Sqlmap;
@@ -18,12 +18,12 @@ public class OxmSqlService implements SqlService {
         oxmSqlReader.setUnmarshaller(unmarshaller);
     }
 
-    public void setSqlmapFile(String sqlmapFile) {
-        oxmSqlReader.setSqlmapFile(sqlmapFile);
-    }
-
     public void setSqlRegistry(SqlRegistry sqlRegistry) {
         this.sqlRegistry = sqlRegistry;
+    }
+
+    public void setSqlmap(Resource sqlmap) {
+        oxmSqlReader.setSqlmap(sqlmap);
     }
 
     @PostConstruct
@@ -42,27 +42,26 @@ public class OxmSqlService implements SqlService {
     private static class OxmSqlReader implements SqlReader {
         private static final String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
         private Unmarshaller unmarshaller;
-        private String sqlmapFile = DEFAULT_SQLMAP_FILE;
+        private Resource sqlmap = new ClassPathResource(DEFAULT_SQLMAP_FILE);
 
         public void setUnmarshaller(Unmarshaller unmarshaller) {
             this.unmarshaller = unmarshaller;
         }
 
-        public void setSqlmapFile(String sqlmapFile) {
-            this.sqlmapFile = sqlmapFile;
+        public void setSqlmap(Resource sqlmap) {
+            this.sqlmap = sqlmap;
         }
 
         @Override
         public void read(SqlRegistry sqlRegistry) {
             try {
-                Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(new StreamSource(
-                        getClass().getClassLoader().getResourceAsStream(sqlmapFile)));
+                Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(new StreamSource(this.sqlmap.getInputStream()));
 
                 for (SqlType sql : sqlmap.getSql()) {
                     sqlRegistry.registerSql(sql.getKey(), sql.getValue());
                 }
             } catch (IOException e) {
-                throw new IllegalArgumentException(sqlmapFile + "을 가져올 수 없습니다", e);
+                throw new IllegalArgumentException(this.sqlmap.getFilename() + "을 가져올 수 없습니다", e);
             }
         }
     }
