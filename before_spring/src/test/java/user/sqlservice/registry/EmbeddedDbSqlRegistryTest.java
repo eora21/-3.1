a14d4f9ba33b1d69bb4071be109527a361288be8
eora21 <eora21@naver.com>
2023-12-14 17:02:38 +0900

    test: 💍 추가 내장 H2에 대한 테스트 클래스 작성

---
+++ before_spring/src/test/java/user/sqlservice/registry/EmbeddedDbSqlRegistryTest.java
@@ -0,0 +1,29 @@
+package user.sqlservice.registry;
+
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
+    EmbeddedDatabase db;
+
+    @Override
+    protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
+        EmbeddedDatabase db = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:db/schema.sql")
+                .addScript("classpath:db/data.sql")
+                .build();
+
+        EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
+        embeddedDbSqlRegistry.setDataSource(db);
+
+        return embeddedDbSqlRegistry;
+    }
+
+    @AfterEach
+    void tearDown() {
+        db.shutdown();
+    }
+}
