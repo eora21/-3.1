c1a96b8eb830c9e7d04726d9019db5ff458a164e
eora21 <eora21@naver.com>
2023-12-16 08:31:12 +0900

    test: 💍 추가 다중 SQL 업데이트 중 실패 시 트랜잭션 적용을 확인하는 테스트 작성

--- before_spring/src/test/java/user/sqlservice/registry/EmbeddedDbSqlRegistryTest.java
+++ before_spring/src/test/java/user/sqlservice/registry/EmbeddedDbSqlRegistryTest.java
@@ -1,9 +1,17 @@
 package user.sqlservice.registry;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.assertj.core.api.Fail;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import user.sqlservice.exception.SqlUpdateFailureException;
 
 class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
     EmbeddedDatabase db;
@@ -19,6 +27,29 @@ class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
         embeddedDbSqlRegistry.setDataSource(db);
 
         return embeddedDbSqlRegistry;
+    }
+
+    @Test
+    void transactionalUpdate() {
+        assertThat(sqlRegistry.findSql("KEY1")).isEqualTo("SQL1");
+        assertThat(sqlRegistry.findSql("KEY2")).isEqualTo("SQL2");
+        assertThat(sqlRegistry.findSql("KEY3")).isEqualTo("SQL3");
+
+        Map<String, String> sqlmap = Map.of(
+                "KEY1", "Modified1",
+                "KEY9999!@#$", "Modified9999"
+        );
+
+        try {
+            sqlRegistry.updateSql(sqlmap);
+            fail("SqlUpdateFailureException expected");
+        } catch (SqlUpdateFailureException e) {
+            //ignore
+        }
+
+        assertThat(sqlRegistry.findSql("KEY1")).isEqualTo("SQL1");
+        assertThat(sqlRegistry.findSql("KEY2")).isEqualTo("SQL2");
+        assertThat(sqlRegistry.findSql("KEY3")).isEqualTo("SQL3");
     }
 
     @AfterEach
