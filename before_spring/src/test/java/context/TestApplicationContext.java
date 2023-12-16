9f006571f75c702fd7e269374893c381878f89fe
eora21 <eora21@naver.com>
2023-12-16 15:51:25 +0900

    feat: 🎸 추가 DataSource 빈 작성

--- before_spring/src/test/java/context/TestApplicationContext.java
+++ before_spring/src/test/java/context/TestApplicationContext.java
@@ -1,10 +1,25 @@
 package context;
 
+import com.mysql.cj.jdbc.Driver;
+import info.Info;
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 @ImportResource("classpath:testApplicationContext.xml")
 public class TestApplicationContext {
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
 
+        dataSource.setDriverClass(Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/toby_spring");
+        dataSource.setUsername(Info.MYSQL_ID.getValue());
+        dataSource.setPassword(Info.MYSQL_PASSWORD.getValue());
+
+        return dataSource;
+    }
 }
