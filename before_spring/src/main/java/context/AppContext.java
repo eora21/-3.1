2bce81d0262ce3301dda9e4b46106959b3b8dad5
eora21 <eora21@naver.com>
2023-12-16 22:20:54 +0900

    refactor: 💡 수정 SQL 관련된 빈들을 @Import로 건네받아 설정하도록 변경

--- before_spring/src/main/java/context/AppContext.java
+++ before_spring/src/main/java/context/AppContext.java
@@ -7,13 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.mail.MailSender;
-import org.springframework.oxm.Unmarshaller;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import user.dao.UserDao;
@@ -21,14 +18,11 @@ import user.domain.DummyMailSender;
 import user.domain.NormalLevelUpgradePolicy;
 import user.domain.UserLevelUpgradePolicy;
 import user.service.UserService;
-import user.sqlservice.registry.EmbeddedDbSqlRegistry;
-import user.sqlservice.registry.SqlRegistry;
-import user.sqlservice.service.OxmSqlService;
-import user.sqlservice.service.SqlService;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "user")
+@Import(SqlServiceContext.class)
 public class AppContext {
     @Autowired
     UserDao userDao;
@@ -38,12 +32,10 @@ public class AppContext {
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
         dataSource.setDriverClass(Driver.class);
         dataSource.setUrl("jdbc:mysql://localhost/toby_spring");
         dataSource.setUsername(Info.MYSQL_ID.getValue());
         dataSource.setPassword(Info.MYSQL_PASSWORD.getValue());
-
         return dataSource;
     }
 
@@ -52,38 +44,6 @@ public class AppContext {
         DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
         transactionManager.setDataSource(dataSource());
         return transactionManager;
-    }
-
-    @Bean
-    public Unmarshaller unmarshaller() {
-        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-        jaxb2Marshaller.setPackagesToScan("user.sqlservice.jaxb");
-        return jaxb2Marshaller;
-    }
-
-    @Bean
-    public DataSource embeddedDatabase() {
-        return new EmbeddedDatabaseBuilder()
-                .setName("embeddedDatabase")
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript("classpath:db/schema.sql")
-                .generateUniqueName(true)
-                .build();
-    }
-
-    @Bean
-    public SqlRegistry sqlRegistry() {
-        EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
-        embeddedDbSqlRegistry.setDataSource(embeddedDatabase());
-        return embeddedDbSqlRegistry;
-    }
-
-    @Bean
-    public SqlService sqlService() {
-        OxmSqlService oxmSqlService = new OxmSqlService();
-        oxmSqlService.setUnmarshaller(unmarshaller());
-        oxmSqlService.setSqlRegistry(sqlRegistry());
-        return oxmSqlService;
     }
 
     @Bean
