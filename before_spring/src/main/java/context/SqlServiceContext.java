85bbda9113de9ea1687bbc581951bc5dc2bd7a20
eora21 <eora21@naver.com>
2023-12-16 22:19:56 +0900

    feat: 🎸 추가 SQL 관련 빈을 모아둔 컨텍스트 작성

---
+++ before_spring/src/main/java/context/SqlServiceContext.java
@@ -0,0 +1,48 @@
+package context;
+
+import javax.sql.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import user.sqlservice.registry.EmbeddedDbSqlRegistry;
+import user.sqlservice.registry.SqlRegistry;
+import user.sqlservice.service.OxmSqlService;
+import user.sqlservice.service.SqlService;
+
+@Configuration
+public class SqlServiceContext {
+    @Bean
+    public DataSource embeddedDatabase() {
+        return new EmbeddedDatabaseBuilder()
+                .setName("embeddedDatabase")
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:db/schema.sql")
+                .generateUniqueName(true)
+                .build();
+    }
+
+    @Bean
+    public Unmarshaller unmarshaller() {
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        jaxb2Marshaller.setPackagesToScan("user.sqlservice.jaxb");
+        return jaxb2Marshaller;
+    }
+
+    @Bean
+    public SqlRegistry sqlRegistry() {
+        EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
+        embeddedDbSqlRegistry.setDataSource(embeddedDatabase());
+        return embeddedDbSqlRegistry;
+    }
+
+    @Bean
+    public SqlService sqlService() {
+        OxmSqlService oxmSqlService = new OxmSqlService();
+        oxmSqlService.setUnmarshaller(unmarshaller());
+        oxmSqlService.setSqlRegistry(sqlRegistry());
+        return oxmSqlService;
+    }
+}
