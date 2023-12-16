62c5c2bd3ab71b723e96fa0df985f398c9827387
eora21 <eora21@naver.com>
2023-12-16 17:27:17 +0900

    feat: 🎸 추가 나머지 Bean 모두 작성
    embeddedDatabase는 필드 이름으로 Bean을 가져오는 @Resource 어노테이션으로 획득

--- before_spring/src/test/java/context/TestApplicationContext.java
+++ before_spring/src/test/java/context/TestApplicationContext.java
@@ -2,23 +2,35 @@ package context;
 
 import com.mysql.cj.jdbc.Driver;
 import info.Info;
+import jakarta.annotation.Resource;
 import javax.sql.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.MailSender;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import user.dao.UserDao;
 import user.dao.UserDaoJdbc;
+import user.domain.DummyMailSender;
+import user.domain.NormalLevelUpgradePolicy;
+import user.domain.UserLevelUpgradePolicy;
+import user.service.UserService;
+import user.service.UserServiceImpl;
+import user.service.UserServiceTest.TestUserService;
+import user.sqlservice.registry.EmbeddedDbSqlRegistry;
+import user.sqlservice.registry.SqlRegistry;
+import user.sqlservice.service.OxmSqlService;
 import user.sqlservice.service.SqlService;
 
 @Configuration
 @ImportResource("classpath:testApplicationContext.xml")
 public class TestApplicationContext {
-    @Autowired
-    SqlService sqlService;
+    @Resource
+    DataSource embeddedDatabase;
 
     @Bean
     public DataSource dataSource() {
@@ -43,7 +55,55 @@ public class TestApplicationContext {
     public UserDao userDao() {
         UserDaoJdbc userDaoJdbc = new UserDaoJdbc();
         userDaoJdbc.setJdbcTemplate(dataSource());
-        userDaoJdbc.setSqlService(sqlService);
+        userDaoJdbc.setSqlService(sqlService());
         return userDaoJdbc;
+    }
+
+    @Bean
+    public Unmarshaller unmarshaller() {
+        Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
+        jaxb2Marshaller.setPackagesToScan("user.sqlservice.jaxb");
+        return jaxb2Marshaller;
+    }
+
+    @Bean
+    public SqlRegistry sqlRegistry() {
+        EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
+        embeddedDbSqlRegistry.setDataSource(embeddedDatabase);
+        return embeddedDbSqlRegistry;
+    }
+
+    @Bean
+    public SqlService sqlService() {
+        OxmSqlService oxmSqlService = new OxmSqlService();
+        oxmSqlService.setUnmarshaller(unmarshaller());
+        oxmSqlService.setSqlRegistry(sqlRegistry());
+        return oxmSqlService;
+    }
+
+    @Bean
+    public MailSender mailSender() {
+        return new DummyMailSender();
+    }
+
+    @Bean
+    public UserLevelUpgradePolicy userLevelUpgradePolicy() {
+        return new NormalLevelUpgradePolicy(userDao(), mailSender());
+    }
+
+    @Bean
+    public UserService userService() {
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.setUserDao(userDao());
+        userServiceImpl.setLevelUpgradePolicy(userLevelUpgradePolicy());
+        return userServiceImpl;
+    }
+
+    @Bean
+    public UserService testUserService() {
+        TestUserService testUserService = new TestUserService();
+        testUserService.setUserDao(userDao());
+        testUserService.setLevelUpgradePolicy(userLevelUpgradePolicy());
+        return testUserService;
     }
 }
