2ebad9ebf36b0908bed91523b64f0b88baef3b06
eora21 <eora21@naver.com>
2023-12-16 16:08:25 +0900

    feat: 🎸 추가 UserDao 빈 작성
    xml에 선언된 빈을 활용하려면 @Autowired를 이용한다.

--- before_spring/src/test/java/context/TestApplicationContext.java
+++ before_spring/src/test/java/context/TestApplicationContext.java
@@ -3,16 +3,23 @@ package context;
 import com.mysql.cj.jdbc.Driver;
 import info.Info;
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
+import user.dao.UserDao;
+import user.dao.UserDaoJdbc;
+import user.sqlservice.service.SqlService;
 
 @Configuration
 @ImportResource("classpath:testApplicationContext.xml")
 public class TestApplicationContext {
+    @Autowired
+    SqlService sqlService;
+
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
@@ -30,5 +37,13 @@ public class TestApplicationContext {
         DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
         transactionManager.setDataSource(dataSource());
         return transactionManager;
+    }
+
+    @Bean
+    public UserDao userDao() {
+        UserDaoJdbc userDaoJdbc = new UserDaoJdbc();
+        userDaoJdbc.setJdbcTemplate(dataSource());
+        userDaoJdbc.setSqlService(sqlService);
+        return userDaoJdbc;
     }
 }
