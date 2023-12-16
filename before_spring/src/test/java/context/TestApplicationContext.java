2fab658f74d3b5c0f165c64caf5da47258f7ff99
eora21 <eora21@naver.com>
2023-12-16 17:49:58 +0900

    feat: 🎸 추가 내장형 DB 빈 작성

--- before_spring/src/test/java/context/TestApplicationContext.java
+++ before_spring/src/test/java/context/TestApplicationContext.java
@@ -2,13 +2,14 @@ package context;
 
 import com.mysql.cj.jdbc.Driver;
 import info.Info;
-import jakarta.annotation.Resource;
 import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.mail.MailSender;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
@@ -29,9 +30,6 @@ import user.sqlservice.service.SqlService;
 @Configuration
 @ImportResource("classpath:testApplicationContext.xml")
 public class TestApplicationContext {
-    @Resource
-    DataSource embeddedDatabase;
-
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
@@ -67,9 +65,19 @@ public class TestApplicationContext {
     }
 
     @Bean
+    public DataSource embeddedDatabase() {
+        return new EmbeddedDatabaseBuilder()
+                .setName("embeddedDatabase")
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:db/schema.sql")
+                .generateUniqueName(true)
+                .build();
+    }
+
+    @Bean
     public SqlRegistry sqlRegistry() {
         EmbeddedDbSqlRegistry embeddedDbSqlRegistry = new EmbeddedDbSqlRegistry();
-        embeddedDbSqlRegistry.setDataSource(embeddedDatabase);
+        embeddedDbSqlRegistry.setDataSource(embeddedDatabase());
         return embeddedDbSqlRegistry;
     }
 
