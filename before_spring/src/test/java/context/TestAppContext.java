aac550b76c39c7c96b55d09839e6dcb946daebc5
eora21 <eora21@naver.com>
2023-12-16 21:46:46 +0900

    feat: 🎸 추가 테스트용 컨텍스트 작성

---
+++ before_spring/src/test/java/context/TestAppContext.java
@@ -0,0 +1,21 @@
+package context;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailSender;
+import user.domain.DummyMailSender;
+import user.service.UserService;
+import user.service.UserServiceTest.TestUserService;
+
+@Configuration
+public class TestAppContext {
+    @Bean
+    public MailSender mailSender() {
+        return new DummyMailSender();
+    }
+
+    @Bean
+    public UserService testUserService() {
+        return new TestUserService();
+    }
+}
