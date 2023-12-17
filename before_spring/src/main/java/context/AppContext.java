2ac854df1d9925200946240dd888d5cb692642dc
eora21 <eora21@naver.com>
2023-12-17 12:12:09 +0900

    refactor: 💡 수정 Context 중첩 클래스로 변경

--- before_spring/src/main/java/context/AppContext.java
+++ before_spring/src/main/java/context/AppContext.java
@@ -8,9 +8,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import user.dao.UserDao;
@@ -21,7 +23,7 @@ import user.service.UserService;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "user")
-@Import({SqlServiceContext.class, ProductionAppContext.class, LocalAppContext.class})
+@Import(SqlServiceContext.class)
 public class AppContext {
     @Autowired
     UserDao userDao;
@@ -50,5 +52,27 @@ public class AppContext {
     @Bean
     public UserLevelUpgradePolicy userLevelUpgradePolicy(UserDao userDao, MailSender mailSender) {
         return new NormalLevelUpgradePolicy(userDao, mailSender);
+    }
+
+    @Configuration
+    @Profile("production")
+    public static class ProductionAppContext {
+        @Bean
+        public MailSender mailSender() {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("mail.mycompoany.com");
+            return mailSender;
+        }
+    }
+
+    @Configuration
+    @Profile("local")
+    public static class LocalAppContext {
+        @Bean
+        public MailSender mailSender() {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setHost("localhost");
+            return mailSender;
+        }
     }
 }
