08699993c77f905ef384966304fe5db63a110889
eora21 <eora21@naver.com>
2023-12-18 21:12:25 +0900

    test: 💍 추가 빈 메타정보를 빈으로 등록하여 확인하는 테스트 작성

--- 1_IoC_Container_and_DI/src/test/java/com/example/ioc_di/container/ContainerTest.java
+++ 1_IoC_Container_and_DI/src/test/java/com/example/ioc_di/container/ContainerTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.ioc_di.container.pojo.Hello;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.support.StaticApplicationContext;
 
 class ContainerTest {
@@ -14,5 +15,14 @@ class ContainerTest {
 
         Hello hello = context.getBean("hello", Hello.class);
         assertThat(hello).isNotNull();
+
+        RootBeanDefinition beanDefinition = new RootBeanDefinition(Hello.class);
+        beanDefinition.getPropertyValues().addPropertyValue("name", "Spring");  // 빈의 name 프로퍼티에 들어갈 값을 지정
+        context.registerBeanDefinition("hello2", beanDefinition);
+
+        Hello hello2 = context.getBean("hello2", Hello.class);
+        assertThat(hello2).isNotNull();
+
+        assertThat(context.getBeanFactory().getBeanDefinitionCount()).isEqualTo(2);
     }
 }
