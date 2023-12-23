c87c76cbc97c6911f023ff25a211b3ad5ec45e1f
eora21 <eora21@naver.com>
2023-12-23 16:41:16 +0900

    feat: 🎸 추가 서블릿 관련 테스트 인터페이스 작성

---
+++ 3_web_mvc/src/test/java/toby/web_mvc/servlet/AfterRunService.java
@@ -0,0 +1,19 @@
+package toby.web_mvc.servlet;
+
+import java.io.UnsupportedEncodingException;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.ModelAndView;
+
+public interface AfterRunService {
+    String getContentAsString() throws UnsupportedEncodingException;
+
+    WebApplicationContext getContext();
+
+    <T> T getBean(Class<T> beanType);
+
+    ModelAndView getModelAndView();
+
+    AfterRunService assertViewName(String viewName);
+
+    AfterRunService assertModel(String name, Object value);
+}
