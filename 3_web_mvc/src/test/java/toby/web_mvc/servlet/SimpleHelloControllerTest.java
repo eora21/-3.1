f547dba1baf751f374c1367f6920bb6e46e575ce
eora21 <eora21@naver.com>
2023-12-23 17:53:34 +0900

    test: 💍 추가 AbstractDispatcherServletTest를 이용한 체이닝으로 컨트롤러 테스트 작성

---
+++ 3_web_mvc/src/test/java/toby/web_mvc/servlet/SimpleHelloControllerTest.java
@@ -0,0 +1,23 @@
+package toby.web_mvc.servlet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import toby.web_mvc.HelloSpring;
+
+public class SimpleHelloControllerTest extends AbstractDispatcherServletTest {
+    @Test
+    void helloController() throws Exception {
+        ModelAndView mav = setLocations("spring-servlet.xml")
+                .setClasses(HelloSpring.class)
+                .initRequest("/hello", RequestMethod.GET)
+                .addParameter("name", "Spring")
+                .runService()
+                .getModelAndView();
+
+        assertThat(mav.getViewName()).isEqualTo("/WEB-INF/view/hello.jsp");
+        assertThat(mav.getModel().get("message")).isEqualTo("Hello Spring");
+    }
+}
