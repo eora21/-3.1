5793b922412ada1a91b1cf61ae3c26c833823de6
eora21 <eora21@naver.com>
2023-12-24 16:03:59 +0900

    feat: 🎸 추가 필수 파라미터를 기입하는 어노테이션 작성

---
+++ 3_web_mvc/src/main/java/toby/web_mvc/custom/RequiredParams.java
@@ -0,0 +1,11 @@
+package toby.web_mvc.custom;
+
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface RequiredParams {
+    String[] value();
+}
