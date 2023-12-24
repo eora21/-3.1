85ca1eec5cf38cbb4553ea41a3f95586e325b8e3
eora21 <eora21@naver.com>
2023-12-25 08:58:42 +0900

    feat: 🎸 추가 루트 컨텍스트를 만들어 리스너에 등록하도록 작성
    리스너는 지정된 루트 컨텍스트가 없다면 디폴트 루트 컨텍스트를 만든다. 지금처럼
    AnnotationConfigWebApplicationContext를 만들어 넣어줬을 시에는 해당 컨텍스트를 사용한다.

--- 3_web_mvc/src/main/java/toby/web_mvc/init/CustomWebApplicationInitializer.java
+++ 3_web_mvc/src/main/java/toby/web_mvc/init/CustomWebApplicationInitializer.java
@@ -4,11 +4,15 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 public class CustomWebApplicationInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        ContextLoaderListener listener = new ContextLoaderListener();
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+//        rootContext.scan("@Configuration annotation class");
+
+        ContextLoaderListener listener = new ContextLoaderListener(rootContext);
         servletContext.addListener(listener);
     }
 }
