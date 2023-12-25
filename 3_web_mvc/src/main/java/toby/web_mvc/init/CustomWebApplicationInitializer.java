f44c6ea8a89bc6f1f4d8c279c6782e77d35a5110
eora21 <eora21@naver.com>
2023-12-25 09:03:33 +0900

    feat: 🎸 추가 디스패쳐 서블릿 지정하여 사용하도록 작성

--- 3_web_mvc/src/main/java/toby/web_mvc/init/CustomWebApplicationInitializer.java
+++ 3_web_mvc/src/main/java/toby/web_mvc/init/CustomWebApplicationInitializer.java
@@ -2,9 +2,11 @@ package toby.web_mvc.init;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class CustomWebApplicationInitializer implements WebApplicationInitializer {
     @Override
@@ -14,5 +16,10 @@ public class CustomWebApplicationInitializer implements WebApplicationInitialize
 
         ContextLoaderListener listener = new ContextLoaderListener(rootContext);
         servletContext.addListener(listener);
+
+        ServletRegistration.Dynamic dispatcher =
+                servletContext.addServlet("spring", new DispatcherServlet());
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/app/*");
     }
 }
