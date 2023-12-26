5c9d551ad0e1f17b4c2d8dc0a7f06405bf6649aa
eora21 <eora21@naver.com>
2023-12-26 12:41:30 +0900

    feat: 🎸 추가 member의 age에 대해 최소 최대 프로퍼티 에디터를 적용하도록 작성

--- 4_spring_mvc/src/main/java/com/example/spring_mvc/controller/ConvertController.java
+++ 4_spring_mvc/src/main/java/com/example/spring_mvc/controller/ConvertController.java
@@ -1,10 +1,13 @@
 package com.example.spring_mvc.controller;
 
 import com.example.spring_mvc.level.Level;
+import com.example.spring_mvc.member.Member;
 import com.example.spring_mvc.property_editor.LevelPropertyEditor;
+import com.example.spring_mvc.property_editor.MinMaxPropertyEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +16,16 @@ public class ConvertController {
     @InitBinder
     public void initBinder(WebDataBinder dataBinder) {
         dataBinder.registerCustomEditor(Level.class, new LevelPropertyEditor());
+        dataBinder.registerCustomEditor(int.class, "age", new MinMaxPropertyEditor(0, 200));
     }
 
     @GetMapping("/search")
     public String search(@RequestParam("level") Level level) {
         return level.name();
+    }
+
+    @GetMapping("/member")
+    public Member user(@ModelAttribute Member member) {
+        return member;
     }
 }
