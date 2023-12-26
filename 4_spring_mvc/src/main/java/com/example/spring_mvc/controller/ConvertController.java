62ac7adaaa96d1a17fd75173d3e4d519d6456324
eora21 <eora21@naver.com>
2023-12-26 14:10:01 +0900

    refactor: 💡 삭제 레벨 프로퍼티 에디터 제거

--- 4_spring_mvc/src/main/java/com/example/spring_mvc/controller/ConvertController.java
+++ 4_spring_mvc/src/main/java/com/example/spring_mvc/controller/ConvertController.java
@@ -4,6 +4,10 @@ import com.example.spring_mvc.level.Level;
 import com.example.spring_mvc.member.Member;
 import com.example.spring_mvc.property_editor.LevelPropertyEditor;
 import com.example.spring_mvc.property_editor.MinMaxPropertyEditor;
+import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -15,7 +19,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConvertController {
     @InitBinder
     public void initBinder(WebDataBinder dataBinder) {
-        dataBinder.registerCustomEditor(Level.class, new LevelPropertyEditor());
         dataBinder.registerCustomEditor(int.class, "age", new MinMaxPropertyEditor(0, 200));
     }
 
