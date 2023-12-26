272cccc089fe83b086d6c1a38f695c5efcddd606
eora21 <eora21@naver.com>
2023-12-26 14:57:50 +0900

    refactor: 💡 수정 converter 빈 등록 해제
    Formatter 적용을 위함

--- 4_spring_mvc/src/main/java/com/example/spring_mvc/converter/LevelToStringConverter.java
+++ 4_spring_mvc/src/main/java/com/example/spring_mvc/converter/LevelToStringConverter.java
@@ -4,7 +4,7 @@ import com.example.spring_mvc.level.Level;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class LevelToStringConverter implements Converter<Level, String> {
     @Override
     public String convert(Level level) {
--- 4_spring_mvc/src/main/java/com/example/spring_mvc/converter/StringToLevelConverter.java
+++ 4_spring_mvc/src/main/java/com/example/spring_mvc/converter/StringToLevelConverter.java
@@ -4,7 +4,7 @@ import com.example.spring_mvc.level.Level;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class StringToLevelConverter implements Converter<String, Level> {
     @Override
     public Level convert(String text) {
