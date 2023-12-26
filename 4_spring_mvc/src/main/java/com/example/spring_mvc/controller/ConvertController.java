d83a8d4d0bb147af1666cef7016ce338ebaac859
eora21 <eora21@naver.com>
2023-12-26 11:06:20 +0900

    feat: 🎸 추가 @InitBinder로 level 파라미터로 들어오는 String값을 Level객체로 변환

--- 4_spring_mvc/src/main/java/com/example/spring_mvc/controller/ConvertController.java
+++ 4_spring_mvc/src/main/java/com/example/spring_mvc/controller/ConvertController.java
@@ -1,12 +1,20 @@
 package com.example.spring_mvc.controller;
 
 import com.example.spring_mvc.level.Level;
+import com.example.spring_mvc.property_editor.LevelPropertyEditor;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ConvertController {
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        dataBinder.registerCustomEditor(Level.class, new LevelPropertyEditor());
+    }
+
     @GetMapping("/search")
     public String search(@RequestParam("level") Level level) {
         return level.name();
