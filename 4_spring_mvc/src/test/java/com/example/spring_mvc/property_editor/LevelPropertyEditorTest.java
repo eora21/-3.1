8ebd633d758cbf9c99af1254a9d94029045894ae
eora21 <eora21@naver.com>
2023-12-26 11:09:20 +0900

    test: 💍 추가 WebDataBinder로 등록된 LevelPropertyEditor 동작 확인 테스트 작성

--- 4_spring_mvc/src/test/java/com/example/spring_mvc/property_editor/LevelPropertyEditorTest.java
+++ 4_spring_mvc/src/test/java/com/example/spring_mvc/property_editor/LevelPropertyEditorTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.example.spring_mvc.level.Level;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.WebDataBinder;
 
 class LevelPropertyEditorTest {
     LevelPropertyEditor levelEditor = new LevelPropertyEditor();
@@ -18,5 +19,12 @@ class LevelPropertyEditorTest {
     void levelToString() {
         levelEditor.setValue(Level.BRONZE);
         assertThat(levelEditor.getAsText()).isEqualTo("1");
+    }
+
+    @Test
+    void webDataBinder() {
+        WebDataBinder dataBinder = new WebDataBinder(null);
+        dataBinder.registerCustomEditor(Level.class, new LevelPropertyEditor());
+        assertThat(dataBinder.convertIfNecessary("1", Level.class)).isEqualByComparingTo(Level.BRONZE);
     }
 }
