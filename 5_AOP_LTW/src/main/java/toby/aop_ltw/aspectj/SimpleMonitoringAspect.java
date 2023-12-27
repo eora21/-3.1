c732b6589b61a70a597bd4a9035c8e38a4cd482e
eora21 <eora21@naver.com>
2023-12-28 08:45:40 +0900

    refactor: 💡 수정 어드바이스 메서드 애노테이션에 포인트컷 표현식 정의로 변경

--- 5_AOP_LTW/src/main/java/toby/aop_ltw/aspectj/SimpleMonitoringAspect.java
+++ 5_AOP_LTW/src/main/java/toby/aop_ltw/aspectj/SimpleMonitoringAspect.java
@@ -3,17 +3,12 @@ package toby.aop_ltw.aspectj;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class SimpleMonitoringAspect {
-    @Pointcut("execution(* hello(..))")
-    private void all() {
-    }
-
-    @Around("all()")
+    @Around("execution(* hello(..))")
     public Object printParametersAndReturnVal(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("before");
         Object ret = pjp.proceed();
