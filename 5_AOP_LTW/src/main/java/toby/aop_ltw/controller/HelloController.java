24718cdba65325f3e837de4a699e1d39a1b6d0ac
eora21 <eora21@naver.com>
2023-12-28 16:51:33 +0900

    feat: 🎸 추가 HelloDto를 입력받는 GetMapping 작성

--- 5_AOP_LTW/src/main/java/toby/aop_ltw/controller/HelloController.java
+++ 5_AOP_LTW/src/main/java/toby/aop_ltw/controller/HelloController.java
@@ -2,12 +2,19 @@ package toby.aop_ltw.controller;
 
 import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
+import toby.aop_ltw.dto.HelloDto;
 
 @RestController
 public class HelloController extends SuperController implements Hello {
     @GetMapping("/hello")
     public Map<String, String> hello() {
         return Map.of("greeting", "hello");
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloDto helloWithDto(@ModelAttribute HelloDto helloDto) {
+        return helloDto;
     }
 }
