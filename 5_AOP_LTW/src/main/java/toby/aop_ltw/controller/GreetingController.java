87f523b0d047c09ded911890209c7846b892008e
eora21 <eora21@naver.com>
2023-12-28 19:53:24 +0900

    refactor: 💡 수정 name이 err라면 예외를 발생시키도록 변경

--- 5_AOP_LTW/src/main/java/toby/aop_ltw/controller/GreetingController.java
+++ 5_AOP_LTW/src/main/java/toby/aop_ltw/controller/GreetingController.java
@@ -11,6 +11,14 @@ public class GreetingController {
     public String greeting(@ModelAttribute HelloDto helloDto) {
         System.out.println();
         System.out.println("=========================");
+
+        if ("err".equals(helloDto.name())) {
+            System.out.println("Exception Throw");
+            System.out.println("=========================");
+            System.out.println();
+            throw new IllegalArgumentException();
+        }
+
         System.out.println("GreetingController Active");
         System.out.println("=========================");
         System.out.println();
