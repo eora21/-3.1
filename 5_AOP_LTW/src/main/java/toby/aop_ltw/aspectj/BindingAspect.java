54926c3b5ca8d6f2cc4982f155440dab7c19f1e6
eora21 <eora21@naver.com>
2023-12-28 21:14:08 +0900

    feat: 🎸 추가 포인트컷 따로 작성 및 어드바이스 지정 어노테이션 전달받도록 작성

--- 5_AOP_LTW/src/main/java/toby/aop_ltw/aspectj/BindingAspect.java
+++ 5_AOP_LTW/src/main/java/toby/aop_ltw/aspectj/BindingAspect.java
@@ -1,7 +1,9 @@
 package toby.aop_ltw.aspectj;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 import toby.aop_ltw.annotation.HelloAnnotation;
 
@@ -11,5 +13,14 @@ public class BindingAspect {
     @Before("@within(helloAnnotation)")
     public void before(HelloAnnotation helloAnnotation) {
         System.out.println("before");
+    }
+
+    @Pointcut("@within(helloAnnotation)")
+    private void withinHelloAnnotation(HelloAnnotation helloAnnotation) {
+    }
+
+    @After(value = "withinHelloAnnotation(helloAnnotation)", argNames = "helloAnnotation")
+    public void after(HelloAnnotation helloAnnotation) {
+        System.out.println("after");
     }
 }
